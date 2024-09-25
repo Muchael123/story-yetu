8746f2eec165677d0a99ebb2af22ad3b1bb4610a
Muchael123 <mainamichael2849@gmail.com>
2024-09-25 14:22:40 +0300

    fix(Feature): fix protected routes

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,9 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/dashboard(.*)", "/create(.*)"]);
+const isProtectedRoute = createRouteMatcher([
+  "/dashboard(.*)",
+  "/create(.*), /explore(.*)",
+]);
 
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect();
