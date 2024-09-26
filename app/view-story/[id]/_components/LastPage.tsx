6c658bf5226ec8e636089b8acdfe82ee76ff7f48
Muchael123 <mainamichael2849@gmail.com>
2024-09-26 14:30:40 +0300

    add social links

--- app/view-story/[id]/_components/LastPage.tsx
+++ app/view-story/[id]/_components/LastPage.tsx
@@ -2,9 +2,11 @@ import { Button } from '@nextui-org/button'
 import { useRouter } from 'next/navigation';
 import React, { useState } from 'react'
 import "react-social-icons/meetup";
-import { SocialIcon } from 'react-social-icons';
-
+import { FaXTwitter } from "react-icons/fa6";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { usePathname } from "next/navigation";
+import { FaLink, FaWhatsapp } from 'react-icons/fa';
 
 function LastPage() {
   const router = useRouter();
@@ -12,7 +14,11 @@ function LastPage() {
   const shareUrl = `${process.env.NEXT_PUBLIC_APP_URL}${pathname}`;
   const title = "Check out this story I just read on Story-Yetu";
   const [showShare, setShowShare] = useState(false);
-
+  const notify = (message:string) => toast.success(message);
+  const copy =async () => {
+    await navigator.clipboard.writeText(shareUrl);
+    notify("Link copied to clipboard");
+  }
 
 
   return (
@@ -41,17 +47,21 @@ function LastPage() {
       </div>
 
       {showShare && (
-        <div className="grid  grid-cols-3 md:grid-cols-4 gap-5 "></div>
+        <div className="grid  grid-cols-3 md:grid-cols-4 gap-5 ">
+          <a
+            target="_blank"
+            href={`https://x.com/intent/post?text=${title}&url=${shareUrl}`}
+          >
+            <FaXTwitter className="text-gray-900" size={24} />
+          </a>
+          <a target="_blank" href={`whatsapp://send?text=${title} ${shareUrl}`}>
+            <FaWhatsapp className="text-green-400" size={24} />
+          </a>
+          <div onClick={() => copy()} className='cursor-pointer'>
+            <FaLink className="text-gray-400" size={24} />
+          </div>
+        </div>
       )}
-      <a
-        href={`https://www.x.com/share?text=ttext=${title}&url=${shareUrl}`}
-      >
-        Twitter
-        {/* <SocialIcon
-              url="www.meetup.com"
-              href={`whatsapp://send?text=${title} ${shareUrl}`}
-            /> */}
-      </a>
     </div>
   );
 }
