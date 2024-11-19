6f59ab87d3374aaca3aa761a44778d07d7e129d2
Muchael123 <mainamichael2849@gmail.com>
2024-11-19 17:00:20 +0300

    Add landing page animations

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,6 +5,7 @@ import StoryList from './_components/StoryList'
 function Dashboard() {
   return (
     <div className='px-10 md:px-20 lg:px-40'>
+      <title>My Site: Contact Us</title>
       <DashboardHeader />
       <StoryList  />
     </div>
